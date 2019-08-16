6cd2b81f2ed8f13510d811aaf8ff217144d9d89e
OverHash <camwoo123@gmail.com>
2019-08-16 22:03:47 +1200

    Created embed with bot latency and API latency

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,7 @@
+const Discord = require('discord.js');
+
+const getUptime = require('../functions/getUptime.js');
+
 module.exports = {
 	name: 'ping',
 	aliases: [ 'lag' ],
@@ -5,6 +9,16 @@ module.exports = {
 	example: 'ping',
 	permissionRequired: 'all',
 	execute(message, args) {
-		message.channel.send(`pong! (**${Math.round(message.client.ping)} ms**)`);
+		message.channel.send('pinging...').then(m => {
+			const embed = new Discord.RichEmbed()
+				.setAuthor('Pong!')
+				.setColor('#3395D6')
+
+				.addField('Bot Latency', (m.createdTimestamp - message.createdTimestamp) + 'ms', true)
+				.addField('API latency', Math.round(message.client.ping) + 'ms', true)
+
+				.setFooter(`Uptime: ${getUptime(message.client)}`);
+			m.edit(embed);
+		});
 	},
 };
