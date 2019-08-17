52e4d48a55320f8ef3d98af28d3b3041c42fd0d9
OverHash <camwoo123@gmail.com>
2019-08-17 13:23:39 +1200

    Fixed bot responding with message when the user was just saying something (e.g. $help to get commands)

--- commands/help.js
+++ commands/help.js
@@ -32,7 +32,7 @@ module.exports = {
 		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
 		if (!command) {
-			return message.reply('that\'s not a valid command!');
+			return;
 		}
 
 		data.push(`**Name:** ${command.name}`);
